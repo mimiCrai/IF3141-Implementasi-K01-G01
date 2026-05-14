7a4049ebe779904089d4cf68706bcb524153adeb
shanlie20 <19623105@std.stei.itb.ac.id>
2026-05-15 02:41:39 +0700

    fix: reporting dan user management

--- custom_addons/reveuse_resto/models/pengguna.py
+++ custom_addons/reveuse_resto/models/pengguna.py
@@ -6,8 +6,8 @@ class Pengguna(models.Model):
 
     name = fields.Char(string='Nama Lengkap', required=True) 
     id_pengguna = fields.Char(string='ID Pengguna', required=True)    
-    user_id = fields.Many2one('res.users', string="User Akun") # Tetap ada di DB, tapi nanti disembunyikan dari layar
-    email = fields.Char(string='Email (Login)', required=True) # Jadikan required buat login
+    user_id = fields.Many2one('res.users', string="User Akun")
+    email = fields.Char(string='Email', required=True)
     no_telp = fields.Char(string='Nomor Telepon')
 
     peran = fields.Selection([
@@ -19,10 +19,8 @@ class Pengguna(models.Model):
     password = fields.Char(string='Password', required=True)
     status_notif = fields.Boolean(string='Terima Notifikasi Minimum', default=False)
 
-    # 1. FUNGSI OTOMATIS BIKIN AKUN SAAT DATA BARU DIBUAT
     @api.model
     def create(self, vals):
-        # 1. Simpan data staf ke tabel custom kita (Reveuse Resto)
         record = super(Pengguna, self).create(vals)
         
         group_map = {
@@ -32,24 +30,20 @@ class Pengguna(models.Model):
             'manager': 'reveuse_resto.group_assistant_manager',
         }
 
-        # 2. Bikin akun Odoo pakai ID Pengguna sebagai Username Login
         if record.id_pengguna and record.peran in group_map:
             group = self.env.ref(group_map[record.peran], raise_if_not_found=False)
             
             new_user = self.env['res.users'].create({
                 'name': record.name,
-                'login': record.id_pengguna, # <-- KUNCI PERUBAHAN: Pakai ID Pengguna buat login
+                'login': record.id_pengguna, # login pakai ini (sebagai emailnya)
                 'password': record.password,
-                'email': record.email,       # <-- Simpan email asli di data user Odoo juga (opsional tapi bagus)
+                'email': record.email,
                 'groups_id': [(4, group.id)] if group else []
-            })
-            
-            # 3. Link akun Odoo ke profil staf
+            })            
             record.user_id = new_user.id
             
         return record
 
-    # 2. FUNGSI OTOMATIS UPDATE GRUP KALAU ROLE DIGANTI (Yang kemarin)
     @api.onchange('peran')
     def _sync_user_groups(self):
         for record in self:
