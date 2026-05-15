01f25316e3f6e11dc76970de9997dd67bb4953c8
shanlie20 <19623105@std.stei.itb.ac.id>
2026-05-15 13:25:12 +0700

    fix: remove unnecessary button

--- custom_addons/reveuse_resto/models/management_report.py
+++ custom_addons/reveuse_resto/models/management_report.py
@@ -7,16 +7,6 @@ class ManagementReport(models.TransientModel):
     tanggal_mulai = fields.Date(string='Periode Mulai', required=True)
     tanggal_akhir = fields.Date(string='Periode Akhir', required=True)
 
-    def action_view_report(self):
-        self.ensure_one()
-        return {
-            'name': 'Stok Bahan Baku Saat Ini',
-            'type': 'ir.actions.act_window',
-            'res_model': 'reveuse_resto.bahan_baku',
-            'view_mode': 'tree,form',
-            'target': 'current',
-        }
-
     def action_print_pdf(self):
         self.ensure_one()
         return self.env.ref('reveuse_resto.action_report_stok_pdf').report_action(self)
